e93784b755feddb0ac15e6f95dae9db343fad7d7
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-08-23 17:03:20 +0600

    complete installation modernizer

--- completed.php
+++ completed.php
@@ -10,4 +10,21 @@ try {
 }
 ?>
 
-Successfully completed your installation !
+<div>
+    <p>Successfully completed your installation !</p>
+    <p>Redirecting(<span id="timer">5</span>s) to main site.. </p>
+</div>
+<script type="text/javascript">
+    var initTimer = 5;
+    var timerElem = document.getElementById("timer");
+    
+    var interval = setInterval(function() {
+        initTimer--;
+        timerElem.innerHTML = initTimer;
+
+        if (initTimer < 1) {
+            clearInterval(interval);
+            window.location.href = "../../";
+        }
+    }, 1000);
+</script>
