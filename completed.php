b67b7efaccf578446b843f0d53610c7d1d85beea
Md Jahid Khan Limon <43162434+mjk-limon@users.noreply.github.com>
2022-11-30 16:23:28 +0600

    Modrn: complete

--- completed.php
+++ completed.php
@@ -24,7 +24,7 @@ var interval = setInterval(function() {
 
     if (initTimer < 1) {
         clearInterval(interval);
-        window.location.href = "../../";
+        window.location.href = "../../../";
     }
 }, 1000);
 </script>
